4d37d846a8b896111d8f8313114edc4a7702649b
root <root@d59b056a8da1>
2020-11-10 23:30:23 +0000

    :)

---
+++ src/_03_print_and_popups/_1_greeter/Greeter.java
@@ -0,0 +1,12 @@
+package _03_print_and_popups._1_greeter;
+
+public class Greeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	syso	
+		JOptionPane.showInputDialog("What is your name?");
+		JOptionPane.showMessageDialog(null,"Hello "+input);
+	}
+
+}
